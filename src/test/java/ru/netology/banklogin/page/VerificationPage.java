3543cec85d35374e63870975aa34e0fcd9186147
tatiana dalvadiants <eparis@atlassian.com>
2023-05-30 14:16:56 +0400

    init commit

---
+++ src/test/java/ru/netology/banklogin/page/VerificationPage.java
@@ -0,0 +1,31 @@
+package ru.netology.banklogin.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Selenide.page;
+import static jdk.internal.org.objectweb.asm.util.CheckClassAdapter.verify;
+
+public class VerificationPage {
+    @FindBy(css = "[data-test-id=code]input")
+    private SelenideElement codeField;
+    @FindBy(css = "[data-test-id=action-verify]")
+    private SelenideElement verifyButton;
+    @FindBy(css = "[data-test-id='error-notification']")
+    private SelenideElement errorNotification;
+    public void verifyVerificationPageVisibility() {
+        codeField.shouldBe(Condition.visible);
+    }
+    public void verifyErrorNotificationVisibility() {
+        errorNotification.shouldBe(Condition.visible);
+    }
+    public DashboardPage validVerify(String verificationCode){
+        verify(verificationCode);
+        return page(DashboardPage.class);
+    }
+    public void verify(String  verificationCode) {
+        codeField.setValue(verificationCode);
+        verifyButton.click();
+    }
+}
